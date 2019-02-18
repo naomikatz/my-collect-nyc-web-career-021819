5b0fe69841e53f626df57478e13dbe88c122b74b
Naomi Katz <naykatz@gmail.com>
2019-02-18 03:41:17 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
 
+   i = 0
+  modified_collection = []
+  while i < collection.length
+    modified_collection << yield(collection[i])
+    i += 1
+  end
+  return modified_collection
+end
 
